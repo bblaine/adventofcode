26d281d4ee21826a12c68b029737e959e5f69ff5
Brian Blaine <briandblaine@gmail.com>
2022-12-06 15:38:52 -0800

    Day 2 solution and tests

--- src/main/scala/2022/Day2.scala
+++ src/main/scala/2022/Day2.scala
@@ -1,7 +1,41 @@
-def topThreeCalories(data: List[List[Int]]) = {
+import scala.io.Source
 
-  val topThreeCalories = data.map(xs => xs.sum).sorted.reverse.take(3).sum
+def loadData2(fileName: String): List[Array[String]] = {
 
-  topThreeCalories
+  val lines = Source.fromResource(fileName).getLines.toList
 
+  val data = lines.map(line => line.split(" "))
+
+  data
+
+}
+
+def totalScore(xs: List[Array[String]]): Int = {
+
+  def roundScore(ys: Array[String]): Int = ys match {
+    case Array("A", "X") => 1 + 3
+    case Array("A", "Y") => 2 + 6
+    case Array("A", "Z") => 3 + 0
+    case Array("B", "X") => 1 + 0
+    case Array("B", "Y") => 2 + 3
+    case Array("B", "Z") => 3 + 6
+    case Array("C", "X") => 1 + 6
+    case Array("C", "Y") => 2 + 0
+    case Array("C", "Z") => 3 + 3
+  }
+
+  xs.map(x => roundScore(x)).sum
+
+}
+
+def elfStrategy(xs: Array[String]): Array[String] = xs match {
+    case Array("A", "X") => Array("A", "Z")
+    case Array("A", "Y") => Array("A", "X")
+    case Array("A", "Z") => Array("A", "Y")
+    case Array("B", "X") => Array("B", "X")
+    case Array("B", "Y") => Array("B", "Y")
+    case Array("B", "Z") => Array("B", "Z")
+    case Array("C", "X") => Array("C", "Y")
+    case Array("C", "Y") => Array("C", "Z")
+    case Array("C", "Z") => Array("C", "X")
 }
--- src/test/scala/2022.scala
+++ src/test/scala/2022.scala
@@ -14,9 +14,16 @@ class Advent2022Suite extends munit.FunSuite {
   }
 
   test("Day2") {
-    val data = loadData("day2-sample")
+    val data = loadData2("day2-sample")
     val obtained = totalScore(data)
     val expected = 15
+    assertEquals(obtained, expected)
+  }
+
+  test("Day2-2") {
+    val data = loadData2("day2-sample").map(x => elfStrategy(x))
+    val obtained = totalScore(data)
+    val expected = 12
     assertEquals(obtained, expected)
   }
 }
