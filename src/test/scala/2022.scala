b348ec6636ace14ab3613e56398c01ed548e1680
Brian Blaine <briandblaine@gmail.com>
2022-12-06 13:52:20 -0800

    Changed data file names

--- src/test/scala/2022.scala
+++ src/test/scala/2022.scala
@@ -1,13 +1,13 @@
 class Advent2022Suite extends munit.FunSuite {
   test("Day1") {
-    val data = loadData("sample")
+    val data = loadData("day1-sample")
     val obtained = maxCalories(data)
     val expected = 24000
     assertEquals(obtained, expected)
   }
 
   test("Day2") {
-    val data = loadData("sample")
+    val data = loadData("day1-sample")
     val obtained = topThreeCalories(data)
     val expected = 45000
     assertEquals(obtained, expected)
